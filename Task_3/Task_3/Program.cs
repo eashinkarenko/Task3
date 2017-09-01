6ff3b76968a9f183944083ab1086b694729aa122
Екатерина <katyusha_s_98@mail.ru>
2017-09-01 22:50:12 +0500

    решение

--- Task_3/Task_3/Program.cs
+++ Task_3/Task_3/Program.cs
@@ -8,9 +8,57 @@ namespace Task_3
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //hgfdih
-        }
+
+            // перевод массива строк с координатами в числовые значения
+            static double[] ConvertToInt(string[] MasXY, double[] MasXYDouble)
+            {
+                MasXYDouble[0] = Convert.ToDouble(MasXY[0]);
+                MasXYDouble[1] = Convert.ToDouble(MasXY[1]);
+
+                return MasXYDouble;
+            }
+
+            // вычисление принадлежности 
+            static void IfTrue(ref bool Ok, double[] MasXYDouble)
+            {
+                double X = MasXYDouble[0], Y = MasXYDouble[1];
+
+                if (X >= 0)
+                {
+                    if (X * X + Y * Y <= 1) Ok = true;
+                }
+                else
+                {
+                    if ((Y <= X / 2 + 1) && (Y >= -X / 2 - 1)) Ok = true;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                string[] MasXY = new string[2];
+                double[] MasXYDouble = new double[2];
+                bool Ok = false;
+                
+                
+                Console.WriteLine("Введите координаты точки серез пробел");
+
+                MasXY = Console.ReadLine().Split(' ');
+
+                try
+                {
+                    ConvertToInt(MasXY, MasXYDouble);
+                    IfTrue(ref
+                    Ok, MasXYDouble);
+                    Console.WriteLine(Ok);
+                    Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверный ввод координат");
+                    Console.ReadLine();
+                }
+            }
+        
+    
     }
 }
